6f128bbeac432c45c56ac60a107776137a81c05b
course <course@course>
2018-02-27 15:06:30 +0000

    Some minor updates. isClustered not needed anymore. REST instead of GRPC

--- accs-cache-sample/src/main/java/nl/amis/smeetsm/springboot/AccsCacheSampleApplication.java
+++ accs-cache-sample/src/main/java/nl/amis/smeetsm/springboot/AccsCacheSampleApplication.java
@@ -1,5 +1,7 @@
 package nl.amis.smeetsm.springboot;
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AccsCacheSampleApplication {
 
 	public static void main(String[] args) {
+		//handy for ACCS logging
+		Map<String, String> env = System.getenv();
+		for (String envName : env.keySet()) {
+		    System.out.format("%s=%s%n", envName, env.get(envName));
+		}
 		SpringApplication.run(AccsCacheSampleApplication.class, args);
 	}
 }
