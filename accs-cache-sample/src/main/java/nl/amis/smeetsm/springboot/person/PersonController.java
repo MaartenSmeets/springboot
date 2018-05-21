6f706d2b04c80ad15f886049928cfdac898570d3
Maarten Smeets <maarten.smeets@amis.nl>
2018-05-21 14:49:22 +0200

    Removing Swagger

--- accs-cache-sample/src/main/java/nl/amis/smeetsm/springboot/person/PersonController.java
+++ accs-cache-sample/src/main/java/nl/amis/smeetsm/springboot/person/PersonController.java
@@ -15,11 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.oracle.cloud.cache.metrics.CacheMetrics;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-
 @RestController
-@Api(value="persons", description="Operations pertaining to persons")
 public class PersonController {
 	
 	private static final Logger log = LoggerFactory.getLogger(PersonController.class);
@@ -27,43 +23,36 @@ public class PersonController {
 	@Autowired
 	private PersonService personService;
 
-	@ApiOperation(value = "View a list persons",response = List.class)
 	@RequestMapping(method=RequestMethod.GET,value="/persons")
 	public List<Person> getAllPersons() {
 		return personService.getAllPersons();
 	}
 	
-	@ApiOperation(value = "Get a person by id",response = Person.class)
     @RequestMapping(method=RequestMethod.GET,value="/persons/{id}")
 	public Person getPerson(@PathVariable Long id) {
 		return personService.getPerson(id);
 	}
 	
-	@ApiOperation(value = "Delete a person by id")
 	@RequestMapping(method=RequestMethod.DELETE,value="/persons/{id}")
 	public void deletePerson(@PathVariable Long id) {
 		personService.deletePerson(id);
 	}
     
-	@ApiOperation(value = "Delete all persons")
 	@RequestMapping(method=RequestMethod.DELETE,value="/persons")
 	public void deletePerson() {
 		personService.deletePersons();
 	}
 	
-	@ApiOperation(value = "Add a person", response = Person.class)
     @RequestMapping(method=RequestMethod.POST,value="/persons")
 	public Person addPerson(@RequestBody Person Person) {
 		return personService.addPerson(Person);
 	}
 
-	@ApiOperation(value = "Update a person",response = Person.class)
 	@RequestMapping(method=RequestMethod.PUT,value="/persons/{id}")
 	public Person addPerson(@RequestBody Person Person,@PathVariable Long id) {
 		return personService.updatePerson(id,Person);
 	}
 	
-	@ApiOperation(value = "Metrics",response = CacheMetrics.class)
 	@RequestMapping(method=RequestMethod.GET,value="/metrics")
 	public CacheMetrics getMetrics() {
 		return personService.getMetrics();
