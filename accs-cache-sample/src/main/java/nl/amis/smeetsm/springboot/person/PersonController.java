c80c334a39c73a38d1e0d04faefb03c72bb9071f
Maarten Smeets <maarten.smeets@amis.nl>
2018-06-03 14:02:35 +0200

    Adding SpringFox API documentation

--- accs-cache-sample/src/main/java/nl/amis/smeetsm/springboot/person/PersonController.java
+++ accs-cache-sample/src/main/java/nl/amis/smeetsm/springboot/person/PersonController.java
@@ -15,6 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.oracle.cloud.cache.metrics.CacheMetrics;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api("Controller for Persons")
 @RestController
 public class PersonController {
 	
@@ -23,31 +27,37 @@ public class PersonController {
 	@Autowired
 	private PersonService personService;
 
+	@ApiOperation("Gets all persons")
 	@RequestMapping(method=RequestMethod.GET,value="/persons")
 	public List<Person> getAllPersons() {
 		return personService.getAllPersons();
 	}
 	
+	@ApiOperation("Gets a person on Id")
     @RequestMapping(method=RequestMethod.GET,value="/persons/{id}")
 	public Person getPerson(@PathVariable Long id) {
 		return personService.getPerson(id);
 	}
 	
+	@ApiOperation("Deletes a person")
 	@RequestMapping(method=RequestMethod.DELETE,value="/persons/{id}")
 	public void deletePerson(@PathVariable Long id) {
 		personService.deletePerson(id);
 	}
     
+	@ApiOperation("Deletes all persons")
 	@RequestMapping(method=RequestMethod.DELETE,value="/persons")
 	public void deletePerson() {
 		personService.deletePersons();
 	}
 	
+	@ApiOperation("Add a person")
     @RequestMapping(method=RequestMethod.POST,value="/persons")
 	public Person addPerson(@RequestBody Person Person) {
 		return personService.addPerson(Person);
 	}
 
+	@ApiOperation("Update a person")
 	@RequestMapping(method=RequestMethod.PUT,value="/persons/{id}")
 	public Person addPerson(@RequestBody Person Person,@PathVariable Long id) {
 		return personService.updatePerson(id,Person);
